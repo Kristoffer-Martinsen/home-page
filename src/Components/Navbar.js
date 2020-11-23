b3120ad87252fa364de244ef32f640b77bea4898
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-11-24 00:04:38 +0100

    Added navbar
    Added navbar component and css. Centered the navbar elements.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../Stylesheets/Navbar.css';
 
 function Navbar() {
     return(
         <div className="Navbar">
-            
+            <h1>About me</h1>
+            <h1>Projects</h1>
+            <h1>Skills</h1>
+            <h1>Contact me</h1>
         </div>
     )
 }
