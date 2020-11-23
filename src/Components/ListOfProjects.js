bbde3f6af7aa9b12dd11966f8a7ab887168a5b55
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-11-23 19:17:29 +0100

    Project Header
    Added a projects header text for the project list section.

--- src/Components/ListOfProjects.js
+++ src/Components/ListOfProjects.js
@@ -6,6 +6,7 @@ import ProjectItem from './ProjectItem';
 function ListOfProjects() {
     return (
             <div className="ListOfProjects">
+                <h1 className="ProjectHeader">Projects:</h1>
                 <ProjectItem nameProject="Collecting Parts" linkToProject="https://github.com/Kristoffer-Martinsen/Collecting-Parts"
                 descriptionProject="My entry in the Ludum Dare 45 Game Jam. The Theme was 
                 starting with nothing. My entry is a platformer where you have to collect all your abilities
