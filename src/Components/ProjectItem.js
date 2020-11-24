b1ba59e396726b3543458d3fedaee18f3a5f1560
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-11-24 19:00:17 +0100

    Updating project item design.
    Added white border. Link no longer changes color when visited.

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -3,12 +3,11 @@ import '../Stylesheets/ProjectItem.css';
 
 function ProjectItem({nameProject, linkToProject, descriptionProject, pathToImage}) {
     return (
-        <div className="ProjectItem">
+        <a href={linkToProject} className="ProjectItem">
             <img src={pathToImage} alt='Screenshot of Collecting Parts'></img>
             <h1>{nameProject}</h1>
-            <a href={linkToProject}>{linkToProject}</a>
             <p>{descriptionProject}</p>
-        </div>
+        </a>
     )
 }
 
