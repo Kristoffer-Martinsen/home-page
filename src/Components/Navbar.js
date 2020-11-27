c3d5973e23ecf0304de4514662590bcfe881d6a4
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-11-27 18:13:10 +0100

    Update Navbar.js
    Added scrolling to specific section when a navbar option is pressed.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../Stylesheets/Navbar.css';
+import {Link} from 'react-scroll';
 
 function Navbar() {
     return(
         <div className="Navbar">
-            <h1>About me</h1>
-            <h1>Projects</h1>
-            <h1>Skills</h1>
-            <h1>Contact me</h1>
+            <h1><Link activeClass="active" to="AboutMe" spy={true} smooth={true}>About me</Link></h1>
+            <h1><Link to="ListOfProjects" spy={true} smooth={true}>Projects</Link></h1>
+            <h1><Link to="Skills" spy={true} smooth={true}>Skills</Link></h1>
+            <h1><Link to="ContactMe" spy={true} smooth={true}>Contact me</Link></h1>
         </div>
     )
 }
