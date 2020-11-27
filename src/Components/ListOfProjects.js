1b77f3a56e06781d3f331b67b27290396889eb27
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-11-27 18:12:31 +0100

    Updated projects
    Added this website as a project. Add min-height to make every project item the same height.

--- src/Components/ListOfProjects.js
+++ src/Components/ListOfProjects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Stylesheets/ListOfProjects.css'
-import CollectingParts from '../Images/CollectingParts.png'
+import CollectingParts from '../Images/CollectingParts.png';
+import Homepage from '../Images/home-page.png';
 import ProjectItem from './ProjectItem';
 
 function ListOfProjects() {
@@ -11,6 +12,9 @@ function ListOfProjects() {
                 descriptionProject="My entry in the Ludum Dare 45 Game Jam. The Theme was 
                 starting with nothing. My entry is a platformer where you have to collect all your abilities
                 from jumping to shooting." pathToImage={CollectingParts}/>
+                <ProjectItem nameProject="This website!" linkToProject="https://github.com/Kristoffer-Martinsen/home-page"
+                descriptionProject="My portfolio website made with ReactJS (create-react-app) where
+                 I show the projects I've made during my software developer journey." pathToImage={Homepage}/>
             </div>
     )
 }
