44fefc3f580d48b97ad0a529abbedad0b5bf078c
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-11-27 18:13:41 +0100

    Updated Skills section
    Added 3 categories in Skills. Updated CSS to reflect this

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -5,13 +5,25 @@ function Skills() {
     return(
         <div className="Skills">
             <h1>Skills:</h1>
-            <ul>
-                <li>Java</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>HTML</li>
-                <li>Git</li>
-            </ul>
+            <div className="Languages">
+                <h2>Languages</h2>
+                <h3>Java</h3>
+                <h3>JavaScript</h3>
+                <h3>HTML5</h3>
+                <h3>CSS</h3>
+            </div>
+            <div className="Technology">
+                <h2>Technology</h2>
+                <h3>Git</h3>
+                <h3>Jira</h3>
+                <h3>Confluence</h3>
+                <h3>Vs Code</h3>
+                <h3>IntelliJ</h3>
+            </div>
+            <div className="Other">
+                <h2>Other</h2>
+                
+            </div>
         </div>
     )
 }
