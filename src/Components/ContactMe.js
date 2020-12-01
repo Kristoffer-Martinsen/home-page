00e37e548f3975c3d42b5f82ad11c65f6c9d2b18
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-12-01 15:18:43 +0100

    Updated ContactMe
    Added header text.Changed color. added padding

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -4,8 +4,11 @@ import '../Stylesheets/ContactMe.css';
 function ContactMe() {
     return (
         <div className="ContactMe">
-            <h3>Phone number: 90102942</h3>
-            <h3>Email: Kristoffermartinsen.95@gmail.com</h3>
+            <h1>Contact Me:</h1>
+            <div>
+                <h3>Phone number: 90102942</h3>
+                <h3>Email: Kristoffermartinsen.95@gmail.com</h3>
+            </div>
         </div>
     )
 }
