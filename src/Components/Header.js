7c7e11582eb14bcc24a9b77c2e24f57c909fc309
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2020-12-01 13:45:11 +0100

    Added socials
    Added social logo library. Added social links to header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { GitHub } from 'react-feather';
+import { Linkedin } from 'react-feather';
 import '../Stylesheets/Header.css';
 
 function Header() {
@@ -6,6 +8,8 @@ function Header() {
         <div className="Header">
             <h1>Kristoffer Martinsen</h1>
             <hr></hr>
+            <a href="https://github.com/Kristoffer-Martinsen"><GitHub/></a>
+            <a href="https://www.linkedin.com/in/kristoffer-martinsen-11b07a167/"><Linkedin/></a>
         </div>
     )
 }
